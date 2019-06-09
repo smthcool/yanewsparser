303518181ce65ad7c1f079de63375115fb382245
smthcool <37275620+smthcool@users.noreply.github.com>
2019-06-09 17:28:44 +0300

    Add files via upload

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(300, 350);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 368);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Прочитать новости";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(488, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(300, 350);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(636, 368);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 25);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Прочитать новости из БД";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(314, 418);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(175, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "https://news.yandex.ru/games.rss";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(256, 392);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(301, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Адрес страницы Яндекс.Новости для парсинга (RSS only)";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 400);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(144, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Загрузить новости в БД";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using MySql.Data.MySqlClient;
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void button1_Click(object sender, EventArgs e) //Выводим новости на экран
+        {
+                string uri = textBox1.Text;
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                string text = null;
+                text = reader.ReadToEnd();
+                string[] separatingChars = { "<title>", "</title>", "<link>", "</link>", "<description>", "</description>", "<pubDate>", "</pubDate>" };
+                string[] words = text.Split(separatingChars, System.StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 11; i < words.Length; i = i + 2)
+                {
+                    richTextBox1.AppendText(words[i] + "\n\n");
+                }
+        }
+        private void button2_Click(object sender, EventArgs e) //Читаем новости из БД
+        {
+            string connStr = "server = localhost; user = root; database = lab; password = ''; Charset = UTF8;";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            conn.Open();
+            string sql = "SELECT  * FROM lab6";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                richTextBox2.AppendText(reader[1].ToString() +  "\n\n"+ reader[2].ToString() + "\n\n" + reader[3].ToString() +"\n\n" + reader[4].ToString() + "\n\n");
+            }
+            reader.Close();
+            conn.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e) //Грузим новости в БД
+        {
+            string uri = textBox1.Text;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+            string text = null;
+            text = reader.ReadToEnd();
+            string[] separatingChars = { "<title>", "</title>", "<link>", "</link>", "<description>", "</description>", "<pubDate>", "</pubDate>" };
+            string[] words = text.Split(separatingChars, System.StringSplitOptions.RemoveEmptyEntries);
+            //Счётчики строк для айди, тайтла, линков, описания, пабдаты
+            int idc = 0;
+            int titlec = 0;
+            int linkc = 2;
+            int descc = 4;
+            int pubdatec = 6;
+            int linesc = 0;
+            string connectionString = "server = localhost; user = root; database = lab; password = ''; Charset = UTF8;";
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+            string query1 = "DELETE FROM lab6 WHERE 1";
+            MySqlCommand comanda = new MySqlCommand(query1, conn);
+            comanda.ExecuteNonQuery();
+            do
+            {
+                string query = "INSERT INTO lab6 ( id, title, link, description, pubdate) VALUES ( @id, @title, @link, @description, @pubdate)";
+                MySqlCommand command = new MySqlCommand(query, conn);
+                command.Parameters.Add("@id", MySqlDbType.Int32).Value = idc;
+                command.Parameters.Add("@title", MySqlDbType.Text).Value = richTextBox1.Lines[titlec];
+                command.Parameters.Add("@link", MySqlDbType.LongText).Value = richTextBox1.Lines[linkc];
+                command.Parameters.Add("@description", MySqlDbType.Text).Value = richTextBox1.Lines[descc];
+                command.Parameters.Add("@pubdate", MySqlDbType.Text).Value = richTextBox1.Lines[pubdatec];
+                command.ExecuteNonQuery();
+                idc = idc + 1;
+                titlec = titlec + 8;
+                linkc = linkc + 8;
+                descc = descc + 8;
+                pubdatec = pubdatec + 8;
+                linesc = linesc + 9;
+            } while (linesc < words.Length);
+            conn.Close();
+        }
+    }
+    }
+
+    
